678dde7dfbf751e9c6e60ebae711cbce8ca18755
10ms88 <mmmsss.88@mail.ru>
2021-02-17 17:45:27 +0300

    add CaptchaCode

--- src/main/java/myBlog/repository/GlobalSettingsRepository.java
+++ src/main/java/myBlog/repository/GlobalSettingsRepository.java
@@ -1,11 +1,11 @@
 package myBlog.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import myBlog.model.GlobalSetting;
 
 @Repository
-public interface GlobalSettingsRepository extends JpaRepository<GlobalSetting, Integer> {
+public interface GlobalSettingsRepository extends PagingAndSortingRepository<GlobalSetting, Integer> {
 
 }
