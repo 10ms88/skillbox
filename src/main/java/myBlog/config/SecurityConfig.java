958cb798e60fdd568eb4ba863940b6777b2a3c8c
10ms88 <mmmsss.88@mail.ru>
2021-03-12 17:37:31 +0300

    correct imageUpload

--- src/main/java/myBlog/config/SecurityConfig.java
+++ src/main/java/myBlog/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package myBlog.config;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +14,11 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -40,8 +46,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     http
         .logout()
-        .logoutSuccessUrl("/login")
-    ;
+        .logoutUrl("/api/auth/logout")
+        .logoutSuccessUrl("/")
+        .deleteCookies("JSESSION");
   }
 
   @Bean
--- src/main/java/myBlog/enumuration/Role.java
+++ src/main/java/myBlog/enumuration/Role.java
@@ -1,12 +1,18 @@
 package myBlog.enumuration;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public enum Role {
-  USER(Set.of(Permission.USER)),
-  MODERATOR(Set.of(Permission.USER, Permission.MODERATE));
+//  USER(Set.of(Permission.USER)),
+//  MODERATOR(Set.of(Permission.USER, Permission.MODERATE));
+  USER(Stream.of(Permission.USER)
+    .collect(Collectors.toCollection(HashSet::new))),
+    MODERATOR(Stream.of(Permission.USER, Permission.MODERATE)
+  .collect(Collectors.toCollection(HashSet::new)));
 
   private final Set<Permission> permissions;
 
